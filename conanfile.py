59d3c3ccb013e1da25209334fc74270637d5a6b2
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-05 12:39:14 -0400

    add build_requires and settings to conanfile

--- conanfile.py
+++ conanfile.py
@@ -3,13 +3,13 @@ from conans import ConanFile, tools, os
 class BoostSystemConan(ConanFile):
     name = "Boost.System"
     version = "1.64.0"
-    generators = "txt"
+    generators = "txt" 
     settings = "os", "arch", "compiler", "build_type"
     url = "https://github.com/boostorg/system"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "system"
-    build_requires = "Boost.Build/1.64.0@bincrafters/testing"
+    build_requires = "Boost.Build/1.64.0@bincrafters/testing" 
     requires =  "Boost.Config/1.64.0@bincrafters/testing", \
                       "Boost.Assert/1.64.0@bincrafters/testing", \
                       "Boost.Core/1.64.0@bincrafters/testing", \
