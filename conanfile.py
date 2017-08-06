e79b06698f31e75c2489b731b793979f8fa9a623
Jerry W <jerrywiltse@gmail.com>
2017-08-05 20:12:58 -0400

    trying boost generator

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,44 @@
-from conans import ConanFile, tools, os
+from conans import ConanFile, os
+
 
 class BoostSystemConan(ConanFile):
     name = "Boost.System"
     version = "1.64.0"
-    generators = "txt" 
+    generators = "txt", "boost"
     settings = "os", "arch", "compiler", "build_type"
     url = "https://github.com/boostorg/system"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "system"
-    build_requires = "Boost.Build/1.64.0@bincrafters/testing" 
-    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Predef/1.64.0@bincrafters/testing", \
-                      "Boost.Winapi/1.64.0@bincrafters/testing"
+    build_requires = "Boost.Build/1.64.0@bincrafters/testing", "Boost.Generator/0.0.1@bincrafters/testing"
+    requires = "Boost.Config/1.64.0@bincrafters/testing", \
+        "Boost.Assert/1.64.0@bincrafters/testing", \
+        "Boost.Core/1.64.0@bincrafters/testing", \
+        "Boost.Predef/1.64.0@bincrafters/testing", \
+        "Boost.Winapi/1.64.0@bincrafters/testing"
                       
     def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
+        self.run("git clone --depth=1 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
+
+    def build(self):
+        boost_build = self.deps_cpp_info["Boost.Build"]
+        b2_bin_name = "b2.exe" if self.settings.os == "Windows" else "b2"
+        b2_bin_dir_name = boost_build.bindirs[0]
+        b2_full_path = os.path.join(boost_build.rootpath, b2_bin_dir_name, b2_bin_name)
+
+        toolsets = {
+          'gcc': 'toolset=gcc',
+          'Visual Studio': 'toolset=msvc',
+          'clang': 'toolset=clang',
+          'apple-clang': 'toolset=darwin'}
+
+        b2_toolset = toolsets[str(self.settings.compiler)]
+        
+        self.run(b2_full_path + " -j4 -a --hash=yes toolset=" + b2_toolset)
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="include", src=include_dir)
+        lib_dir = os.path.join(self.build_folder, "stage/lib")
+        self.copy(pattern="*", dst="lib", src=lib_dir)
