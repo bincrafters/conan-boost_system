ae73bf93458e4a23141d3afa1096c605b6dc7fb9
Jerry W <jerrywiltse@gmail.com>
2017-08-15 00:34:01 -0400

    simplified package step

--- conanfile.py
+++ conanfile.py
@@ -41,8 +41,7 @@ class BoostSystemConan(ConanFile):
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="include", src=include_dir)
-        lib_dir = os.path.join(self.build_folder, "stage/lib")
-        self.copy(pattern="*", dst="lib", src=lib_dir)
+        self.copy(pattern="*", dst="lib", src="stage/lib")
 
     def package_info(self):
         self.cpp_info.libs = self.collect_libs()
