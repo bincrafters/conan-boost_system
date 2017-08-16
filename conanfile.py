a0edf4727aa1ba2cb9752b9b3b5bcb03ca0414b7
Jerry W <jerrywiltse@gmail.com>
2017-08-16 13:35:08 -0400

    changed darwin to clang

--- conanfile.py
+++ conanfile.py
@@ -32,7 +32,7 @@ class BoostSystemConan(ConanFile):
           'gcc': 'gcc',
           'Visual Studio': 'msvc',
           'clang': 'clang',
-          'apple-clang': 'darwin'}
+          'apple-clang': 'clang'}
 
         b2_toolset = toolsets[str(self.settings.compiler)]
         
