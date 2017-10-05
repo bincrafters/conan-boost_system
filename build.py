cab9e8f92bbb4543b311540d20165c09d2ddce0e
Jerry W <jerrywiltse@gmail.com>
2017-10-05 02:57:16 -0400

    add bincrafters remote to force position 0 and optimize

--- build.py
+++ build.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
         os.environ["CONAN_CHANNEL"] = channel
         os.environ["CONAN_REFERENCE"] = "{0}/{1}".format(name, version)
         os.environ["CONAN_UPLOAD"]="https://api.bintray.com/conan/{0}/public-conan".format(username)
+        os.environ["CONAN_REMOTES"]="https://api.bintray.com/conan/{0}/public-conan".format(username)
 
     builder = ConanMultiPackager()
     builder.add_common_builds()    
