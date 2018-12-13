86faf0d758a215de2ad4a723bfdb8bbad6b43db8
Jerry W <jerrywiltse@gmail.com>
2018-12-13 14:48:27 -0500

    fix merge conflicts

--- conanfile.py
+++ conanfile.py
@@ -1,74 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-from conans import ConanFile, tools
-=======
->>>>>>> testing/1.67.0
 
 from conans import python_requires
 
-<<<<<<< HEAD
-class BoostSystemConan(ConanFile):
-    name = "Boost.System"
-    version = "1.65.1"
-    url = "https://github.com/bincrafters/conan-boost-system"
-    lib_short_names = ["system"]
-    is_header_only = False
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-
-    requires = (
-        "boost_package_tools/1.65.1@bincrafters/testing", 
-        "Boost.Config/1.65.1@bincrafters/testing", 
-        "Boost.Assert/1.65.1@bincrafters/testing", 
-        "Boost.Core/1.65.1@bincrafters/testing", 
-        "Boost.Predef/1.65.1@bincrafters/testing", 
-        "Boost.Winapi/1.65.1@bincrafters/testing"
-    )
-
-
-    # BEGIN TEMPLATE
-    
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
-    license = "BSL"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
-    short_paths = True
-    generators = "boost"
-    settings = "os", "arch", "compiler", "build_type"
-
-    def source(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.source(self)
-        getattr(self, "source_additional", lambda:None)()
-
-    def build(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.build(self)
-        getattr(self, "build_additional", lambda:None)()
-        
-    def package(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package(self)
-        getattr(self, "package_additional", lambda:None)()
-        
-    def package_info(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package_info(self)
-        getattr(self, "package_info_additional", lambda:None)()
-            
-    # END TEMPLATE
-=======
-=======
-
-from conans import python_requires
-
->>>>>>> testing/1.68.0
 
 base = python_requires("boost_base/1.68.0@bincrafters/stable")
 
@@ -85,7 +19,3 @@ class BoostSystemConan(base.BoostBaseConan):
         "boost_predef",
         "boost_winapi"
     ]
-<<<<<<< HEAD
->>>>>>> testing/1.67.0
-=======
->>>>>>> testing/1.68.0
