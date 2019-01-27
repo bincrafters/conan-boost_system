664788a4de033b0d87f05052faf5cadafc33eddc
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:36 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostSystemConan(base.BoostBaseConan):
     name = "boost_system"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_system"
     lib_short_names = ["system"]
     options = {"shared": [True, False]}
